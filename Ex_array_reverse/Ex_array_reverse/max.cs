df5839268648238987b4434bd5617382a7f4435e
jooyoung cha <ryuha.kuma@gmail.com>
2023-03-15 22:46:06 +0900

    최댓값, 정렬 문제 등

---
+++ Ex_array_reverse/Ex_array_reverse/max.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_array_reverse
+{
+    class max
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            int answer = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (answer <= numbers[i] * numbers[j])
+                        answer = numbers[i] * numbers[j];
+                }
+            }
+        }
+
+        //다른 풀이
+        void solution()
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+            //내림차순으로 정렬 후 배열로 변환
+            //앞에 두 개 곱하기
+            numbers = numbers.OrderByDescending(x => x).ToArray();
+            int answer = numbers[0] * numbers[1];
+        }
+    }
+}
---
+++ Ex_array_reverse/Ex_array_reverse/string_delete.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_array_reverse
+{
+    class string_delete
+    {
+        static void Main(string[] args)
+        {
+            solution("abcdef", "f");
+        }
+        public static string solution(string my_string, string letter)
+        {
+            string answer = "";
+            for (int i = 0; i < my_string.Length; i++)
+            {
+                if (!my_string[i].Equals(char.Parse(letter)))
+                    answer += my_string[i];
+            }
+            return answer;
+        }
+
+    }
+}
