f1d34e2ae3de615ec4484ade2d7bdd1c8b3dbe6e
jooyoung cha <ryuha.kuma@gmail.com>
2023-03-30 05:19:32 +0900

    11.the gift 관련문제(super computer)
    탐욕 알고리즘
    최적의 해 또는 근사해를 구하는 데 사용하는 방법
    여러 단계로 나누고 각 단계에서 최적의 수를 찾아낸 후 각 단계에서 판단한 최적의 수를 모아 문제의 최종 답을 찾는 알고리즘

---
+++ CodingGame/CodingGame/SuperComputer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingGame
+{
+    class SuperComputer
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[][] arr = new int[N][];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                int J = int.Parse(inputs[0]);
+                int D = int.Parse(inputs[1]);
+                arr[i] = new int[] { J, J+D-1};
+            }
+            var sortedByFirst = arr.OrderBy(x => x[0]);
+            var sortedBySecond = sortedByFirst.OrderBy(x => x[1]);
+            int[][] sortedArr = sortedBySecond.ToArray();
+
+            int day = sortedArr[0][1];
+            int answer = 1;
+            for (int i = 1; i < N; i++)
+            {
+                if (day < sortedArr[i][0])
+                {
+                    day = sortedArr[i][1];
+                    answer++;
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
