86b76e164d0dd1e276e49684970ff09bf15671c3
jooyoung cha <ryuha.kuma@gmail.com>
2023-03-30 03:53:08 +0900

    11.the gift
    탐욕 알고리즘
    각 단계에서 할 수 있는 최선의 수를 생각하고 이를 모아 전체적인 답을 찾아가는 알고리즘
    탐욕 알고리즘은 모든 경우의 수를 확인해야하고 또 그 경우의 수가 기하급수적으로 늘어나는 경우에 고려할 수 있다.

---
+++ CodingGame/CodingGame/thegift.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingGame
+{
+    class thegift
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int[] budget = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                budget[i] = int.Parse(Console.ReadLine());
+            }
+            budget = budget.OrderBy(x => x).ToArray();
+            int remaining = N;
+
+            if (budget.Sum(x => x) >= C)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    int avg = C / remaining;
+                    if (budget[i] < avg)
+                        C -= budget[i];
+                    else
+                        C -= avg;
+                    Console.WriteLine((budget[i]<avg) ? budget[i] : avg);
+                    remaining--;
+                }
+            }
+            else
+                Console.WriteLine("IMPOSSIBLE");
+
+        }
+    }
+}
