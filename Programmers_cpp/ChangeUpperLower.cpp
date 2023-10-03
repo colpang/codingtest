34cb710a6f93a07c09451bbb57b543142deab896
jooyoung cha <ryuha.kuma@gmail.com>
2023-10-04 00:15:25 +0900

    10/3
    1.for문출력
    2.대소문자변경

---
+++ Programmers_cpp/ChangeUpperLower.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    string answer="";
+    for(int i=0; i<str.length(); i++){
+        if(isupper(str[i]))
+            answer+= tolower(str[i]);
+        else
+            answer+=toupper(str[i]);
+    }
+    cout << answer;
+    return 0;
+}
